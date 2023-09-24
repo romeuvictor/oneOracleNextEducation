4893ede8fe9baab168bb33feb7a05d6734584203
Romeu Victor da Rocha <romeuvictor@gmail.com>
2023-09-24 15:42:15 -0300

    Finalizado o 05 curso Java Exececoes

---
+++ 05-Java-Java-lang/bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Tributavel.java
@@ -0,0 +1,7 @@
+package br.com.bytebank.banco.modelo;
+
+public interface Tributavel {
+	
+	abstract double getValorImposto();
+
+}
---
+++ 05-Java-Java-lang/bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -0,0 +1,43 @@
+package br.com.bytebank.banco.test;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		
+		String vazio = " Alura ";
+		
+		String outroVazio = vazio.trim();
+	//	System.out.println(vazio.contains("Alu"));
+	//	System.out.println(outroVazio.isEmpty());
+		Integer a = new Integer(3); 
+		Integer b = new Integer(3); 
+		
+		System.out.println(a==b);
+				
+		
+		//int a = 3;
+		//String nome = "Alura"; //Object Literal
+		//String outro = new String("Alura");
+		
+//		System.out.println(nome.length());		
+//		for(int i = 0; i<nome.length();i++ ) {
+//			System.out.println(nome.charAt(i));
+//		}
+		
+//		String sub = nome.substring(1);
+//		System.out.println(sub);
+		
+//		int pos = nome.indexOf("ur");
+//		System.out.println(pos);	
+		
+//		char c = nome.charAt(2);
+//		System.out.println(c);	
+
+//		String b = nome.replace("A", "a");
+//		b = nome.toLowerCase();
+//		
+//		System.out.println(nome);
+//		System.out.println(b);
+	}
+
+}
